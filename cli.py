f90dba22f45f6fe66f8e61b3cc4ff73db35e2b2e
harusary <uxdx@naver.com>
2021-08-19 15:54:45 +0900

    cli_init

---
+++ cli.py
@@ -0,0 +1,6 @@
+"""Comment Line Interface 실행문
+"""
+
+def create():
+    """첫번째 인자로 create를 받았을 때 실행하는 명령어
+    """
--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+"""요청에 따라 적절한 데이터셋을 반환하는API를 구현
 """
-파라미터로 범위와 간격을 지정하면 
-데이터크기x5 의 행열을 반환하는 api 구현
-"""
+
+import sys
+from data.datasetmaker import PastFutureDataMaker
+from data.marketdata import get_market_data
 
 
 if __name__ == '__main__':
-    pass
+    start_time = '2021-07-14 00:00:00'
+    end_time = '2021-07-16 00:00:00'
+    dataframe = get_market_data(
+        start_time=start_time, end_time=end_time, symbol='BTCUSDT', interval='1h')
+
+    for arg in sys.argv:
+        print(arg)
+
+    if sys.argv[1] == 'create':
+        """데이터셋을 생성.
+        argv[2] : 경로
+        """
