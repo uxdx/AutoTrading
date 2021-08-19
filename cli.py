ce046475c0b2ff2c8fad01f6285f15ec01979438
harusary <uxdx@naver.com>
2021-08-19 16:21:56 +0900

    cli_basic

--- cli.py
+++ cli.py
@@ -1,6 +1,47 @@
 """Comment Line Interface 실행문
 """
 
-def create():
-    """첫번째 인자로 create를 받았을 때 실행하는 명령어
-    """
+
+# Import the click module that was just downloaded.
+import click
+
+
+# Makes the function main a designated group for commands. Allowing multiple functions to be tied to main.
+@click.group()
+# Function that represents a group since there is no command decorator.
+def main():
+	# Creating an empty function since we need to have this exist so we can use it as a group.
+	pass
+
+
+# Uses the "main" group to indicate that this function is a command of that group.
+@main.command()
+@click.option(
+        "--path",
+        "-p",
+        default='./',
+        help="Set a dataset file path."
+)
+def create(path):
+	"""
+	Create dataset in specific path as options
+	"""
+
+	# Simple print statement to the terminal.
+
+	click.echo(path)
+
+
+# Uses the "main" group to indicate that this function is a command of that group.
+@main.command()
+# Function that represents a command. The function name is the command name.
+def delete():
+	"""
+	Prints the value "World!" to the terminal.
+	"""
+	# Simple print statement to the terminal.
+	click.echo("World!")
+
+
+if __name__ == "__main__":
+	main()
--- main.py
+++ main.py
@@ -1,21 +1,5 @@
 """요청에 따라 적절한 데이터셋을 반환하는API를 구현
 """
 
-import sys
-from data.datasetmaker import PastFutureDataMaker
-from data.marketdata import get_market_data
-
-
 if __name__ == '__main__':
-    start_time = '2021-07-14 00:00:00'
-    end_time = '2021-07-16 00:00:00'
-    dataframe = get_market_data(
-        start_time=start_time, end_time=end_time, symbol='BTCUSDT', interval='1h')
-
-    for arg in sys.argv:
-        print(arg)
-
-    if sys.argv[1] == 'create':
-        """데이터셋을 생성.
-        argv[2] : 경로
-        """
+    pass
